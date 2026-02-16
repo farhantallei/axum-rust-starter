a2fc604021771b8db52cb236e93e0fa6032ac7e3
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2026-02-16 16:59:49 +0700

    impl `IntoResponse` on list response

--- src/shared/response.rs
+++ src/shared/response.rs
@@ -1,6 +1,16 @@
+use axum::{Json, response::IntoResponse};
 use serde::Serialize;
 
-#[derive(Debug, Serialize)]
-pub struct ListResponse<T> {
+#[derive(Serialize)]
+pub struct ListResponse<T: Serialize = serde_json::Value> {
     pub data: Vec<T>,
+}
+
+impl<T> IntoResponse for ListResponse<T>
+where
+    T: Serialize,
+{
+    fn into_response(self) -> axum::response::Response {
+        Json(self).into_response()
+    }
 }
